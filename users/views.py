d1db90e6f20af7e73130c3d5ad12e7dce4444ecd
MikolaShobei <mikola.shobey.wr@gmail.com>
2021-03-04 16:19:19 +0200

    qazxsw

--- users/views.py
+++ users/views.py
@@ -9,20 +9,37 @@ db_path = os.path.join("users", 'db.json')
 
 
 def user(request, **kwargs):
+    try:
+        with open(db_path, "r") as file:
+            users_list = [item for item in json.load(file)]
+            users_list.append(kwargs)
+            users_list1 = [User(**item) for item in users_list]
 
-    with open(db_path, "w") as file:
-        json.dump(kwargs, file)
+        json_users_list = json.dumps(users_list)
 
-    with open(db_path, 'r') as file:
+        with open(db_path, "w") as file:
+            file.write(json_users_list)
 
-        users_list = json.load(file)
+    except Exception as err:
+        with open(db_path, 'w') as file:
+            file.write(json.dumps([]))
 
-    return render(request, 'users/user.html', {"users": users_list})
+        with open(db_path, "r") as file:
+            users_list = [item for item in json.load(file)]
+            users_list.append(kwargs)
+            users_list1 = [User(**item) for item in users_list]
 
+        json_users_list = json.dumps(users_list)
+
+        with open(db_path, "w") as file:
+            file.write(json_users_list)
+        print(err)
+    finally:
+        pass
+
+
+    return render(request, 'users/user.html', {"users": users_list1})
 
 
 def home(request):
-
-
-
     return render(request, 'users/home.html')
